162d6f0a5f9ce777dc4d2e2784f17d236823488c
Hampus Olausson <hampus.john.olausson@gmail.com>
2020-12-15 16:17:44 +0100

    fixed the animation on the lines in the hamburger

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,6 +1,8 @@
 "use strict";
 const hamburgerMenu = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-list');
+const navbar = document.querySelector(".nav-section");
 hamburgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('open');
+    navbar.classList.toggle("open");
 });
